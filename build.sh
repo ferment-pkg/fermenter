62be0c72dd85f69bce74d754c40c41de76acd531
NotTimIsReal <alastairrintumathew@gmail.com>
2022-07-04 11:30:12 +1000

    updated build.sh

--- build.sh
+++ build.sh
@@ -5,4 +5,9 @@ GOARCH=amd64 GOOS=darwin go build -o bin/darwin/amd64/fermenter main.go
 echo "Building for arm64..."
 GOARCH=arm64 GOOS=linux go build -o bin/linux/arm64/fermenter main.go
 GOARCH=arm64 GOOS=darwin go build -o bin/darwin/arm64/fermenter main.go
+echo "linking Universal Binary For Macos"
+cd bin/darwin
+lipo -create -output fermenter amd64/fermenter arm64/fermenter
+echo "Removing old binaries"
+rm -rf amd64 arm64
 echo "Done"
